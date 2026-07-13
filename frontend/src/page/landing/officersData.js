2daabefb2cb114e7cfd667585500ef7d615bf8db
clyde-2006 <joreeseclyde@gmail.com>
2026-07-13 21:19:37 +0800

    final 1.5

--- frontend/src/page/landing/officersData.js
+++ frontend/src/page/landing/officersData.js
@@ -2,7 +2,7 @@ export const officersData = {
 
     moderator: {
         initials: "LC",
-        name: "Leonie A. Cajes, MIT",
+        name: "Paul A. Durango",
         position: "Moderator",
         description:
         "Guides discussions and ensures smooth communication."
